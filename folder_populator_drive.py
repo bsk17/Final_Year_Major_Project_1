3e879c8adaccacfc3667724dd0ed43a70a53de64
bsk17 <sk.bijin@gmail.com>
2020-04-16 04:22:08 +0530

    changes made in folder_populator_drive, already uploaded images are skipped now only left images are uploaded

--- folder_populator_drive.py
+++ folder_populator_drive.py
@@ -1,4 +1,4 @@
-# this program creates the folder and populates it with the photographs taken by open cv
+# this program creates the folder and populates it with the photographs taken by open cv if not present in the drive
 
 from __future__ import print_function
 import pickle
@@ -41,25 +41,64 @@ def main():
     # this service will contains the credentials and will help us to communicate with the folders inside drive
     service = build('drive', 'v3', credentials=creds)
 
-    # this is the id of the folder named INVOICE in Colab Notebook in google drive
+    # this is the id of the folder named Captured_Images in Colab Notebook in google drive
     folder_id = '1zLxJ-kUlVP3CX1pWgpVcK_awrsiGMstt'
+
+    # this function will check for the image in Drive already present or not
+    # if present then we skip the image and upload only if it is not present
+    def checkFile(name):
+        found = False
+        # this is to search for the image already present or not
+
+        page_token = None
+
+        while True:
+            response = service.files().list(q="mimeType='image/jpeg' and name =" + "'"+name+"'",
+                                            spaces='drive',
+                                            fields='nextPageToken, files(id, name)',
+                                            pageToken=page_token).execute()
+
+            for file in response.get('files', []):
+                print('Found file: %s (%s)' % (file.get('name'), file.get('id')))
+
+                found = True
+
+            page_token = response.get('nextPageToken', None)
+
+            if page_token is None:
+                break
+
+        if found:
+            return True
+        else:
+            return False
 
     # this is the additional info we provide to this program
     for filename in glob.glob('TrainingImage/*.jpg'):
-        file_metadata = {
-                'name': filename,
+
+        # we call the function with the argument as the file name already present
+        if checkFile(filename.split(" ", 1)[-1]):
+            print("File already present")
+
+        # this means the file is not present so we have to upload
+        else:
+            print("Image "+filename.split(" ", 1)[-1] + " is uploading")
+
+            file_metadata = {
+                # this will take only he name from
+                'name': filename.split(" ", 1)[-1],
                 'parents': [folder_id]
-        }
+            }
 
-        # need to improve this filename for proper uploading of images
-        media = MediaFileUpload(filename,
-                                mimetype='image/jpg',
-                                resumable=True)
-        file = service.files().create(body=file_metadata,
-                                            media_body=media,
-                                            fields='id').execute()
+            media = MediaFileUpload(filename,
+                                    mimetype='image/jpg',
+                                    resumable=True)
 
-        print('File ID: %s' % file.get('id'))
+            file = service.files().create(body=file_metadata,
+                                          media_body=media,
+                                          fields='id').execute()
+
+            print('Uploaded Image ID: %s' % file.get('id'))
 
 
 if __name__ == '__main__':
