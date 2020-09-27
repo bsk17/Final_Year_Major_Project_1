862291576986592e826f97bbb666b5941bc67651
bsk17 <sk.bijin@gmail.com>
2020-09-27 22:56:49 +0530

    NEW ACCOUNT REGISTERED

--- folder_populator_drive.py
+++ folder_populator_drive.py
@@ -51,10 +51,10 @@ def populate():
     service = build('drive', 'v3', credentials=creds)
 
     # this is the id of the folder named Captured_Images in Colab Notebook in google drive(Bijin)
-    folder_id = '1zLxJ-kUlVP3CX1pWgpVcK_awrsiGMstt'
+    # folder_id = '1zLxJ-kUlVP3CX1pWgpVcK_awrsiGMstt'
 
     # this is the id of the folder name Captured Images in Colab Notebook in google drive(Project)
-    # folder_id = '1hrjKfPAk1DerD0zXOOoOxvtCT5qhVMAy'
+    folder_id = '1hrjKfPAk1DerD0zXOOoOxvtCT5qhVMAy'
 
     # this function will check for the image in Drive already present or not
     # if present then we skip the image and upload only if it is not present
